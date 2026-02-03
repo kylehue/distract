097b72a9d4c8eb173118e55980bdaf02205a1e02
kai <apeuda69@gmail.com>
2026-02-03 10:31:49 +0800

    feat: add key.enter support in forms

--- src/app/pages/entry.vue
+++ src/app/pages/entry.vue
@@ -1,6 +1,6 @@
 <template>
    <div class="flex items-center justify-center w-full h-full">
-      <NForm>
+      <NForm @keydown.enter="joinRoom()">
          <NFormItem
             label="Student Name"
             :validation-status="studentNameStatus"
